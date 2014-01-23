81f0a5c2c6712ca1d0a8e019fb0fe77e1050b93f
Mohamad Ihsan <mars.rizta@gmail.com>
2014-01-23 11:04:52 +0700

    Update/Ini yang dipake

--- main.c
+++ main.c
@@ -1,89 +1,77 @@
 #include<stdio.h>
 #include<conio.h>
-#include<conio2.h>
-#include<process.h>
-#include<string.h>
-#include<math.h>
-#define IN 99
-#define N 6 
+#define MAXNODES 50
+#define MAX1 150
+#define INFINITY 5000
 
+//using namespace std;
 
-int dijkstra(int cost[][N], int source, int target);
-int dijsktra(int cost[][N],int source,int target)
-{
-int dist[N],prev[N],selected[N]={0},i,m,min,start,d,j;
-char path[N];
-for(i=1;i< N;i++)
-{
-            dist[i] = IN;
-             prev[i] = -1;
-            }
+int weight[MAXNODES][MAXNODES],i,j,distance[MAXNODES],visit[MAXNODES];
+int precede[MAXNODES],final=0;
+int path[MAX1];
+int smalldist,newdist,k,s,d,current,n,distcurr;
 
-start = source;
-selected[start]=1;
-dist[start] = 0;
-while(selected[target] ==0)
+void Display_Result()
 {
-            min = IN;
-             m = 0;
-             for(i=1;i< N;i++)
-                        {
-                        d = dist[start] +cost[start][i];
-                         if(d< dist[i]&&selected[i]==0)
-                        {
-                        dist[i] = d;
-                         prev[i] = start;
-                        }
-             if(min>dist[i] && selected[i]==0)
-                        {
-                        min = dist[i];
-                         m = i;
-                        }
-            }
-    start = m;
-    selected[start] = 1;
-    }
-
-start = target;
-j = 0;
-while(start != -1)
+i=d;
+path[final]=d;
+final++;
+while(precede[i]!=s)
 {
-            path[j++] = start+65;
-            start = prev[start];
-            }
-path[j]='\0';
-strrev(path);
-printf("%s", path);
-return dist[target];
+  j=precede[i];
+  i=j;
+  path[final]=i;
+  final++;
+}
+path[final]=s;
+printf("\nThe shortest path followed is :\n\n");
+for(i=final;i>0;i--)
+ printf("\t\t(%d -> %d) with cost = %d\n\n",path[i],path[i-1],weight[path[i]][path[i-1]]);
+printf("\nFor total cost = %d",distance[d]);
 }
 
-int main()
+main()
 {
-	
-            int cost[N][N],i,j,w,ch,co;
-            int source, target,x,y;
-            gotoxy(10,2);printf("==== Lintaan Terpendek dengan Algoritma DIJKSTRA ====");
-            gotoxy(25,3);printf("Ada 5 Simpul di Lintasan \n\n\n\n\n");
-			for(i=1;i< N;i++)
-            for(j=1;j< N;j++)
-            cost[i][j] = IN;
-            for(x=1;x< N;x++)
-                        {
-                         for(y=x+1;y< N;y++)
-                                    {
-                                    printf(" Masukkan nilai dari jalur antara simpul %d dan %d : ",x,y);
-                                    scanf("%d",&w);
-                                    cost [x][y] = cost[y][x] = w;
-                                    }
-                        printf("\n");
-                        }
-            printf("\n Masukkan asal simpul: ");
-            scanf("%d", &source);
-            printf("\n Masukkan target simpul: ");
-            scanf("%d", &target);
-            co = dijsktra(cost,source,target);
-            printf("\n Jalur Terpendek: %d",co);
-			
-            getch();     
-            return(0);
+printf("\nEnter the number of nodes(Less than 50)in the matrix : ");
+scanf("%d",&n);
+printf("\nEnter the cost matrix :\n\n");
+for(i=0;i<n;i++)
+  for(j=0;j<n;j++)
+    scanf("%d",&weight[i][j]);
+printf("\nEnter the source node (0 to %d) : ",n-1);
+scanf("%d",&s);
+printf("\nEnter the destination node (0 to %d) : ",n-1);
+scanf("%d",&d); 
+for(i=0;i<n;i++)
+{
+  distance[i]=INFINITY;
+  precede[i]=INFINITY;
+}
+distance[s]=0;
+current=s;
+visit[current]=1;
+while(current!=d)
+{
+  distcurr=distance[current];
+  smalldist=INFINITY;
+  for(i=0;i<n;i++)
+    if(visit[i]==0)
+    {
+      newdist=distcurr+weight[current][i];
+      if(newdist<distance[i])
+      {
+    distance[i]=newdist;
+    precede[i]=current;
+      }
+      if(distance[i]<smalldist)
+      {
+    smalldist=distance[i];
+    k=i;
+      }
+    }
+  current=k;
+  visit[current]=1;
+}
+Display_Result();
+getch();
 }
